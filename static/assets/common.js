533f3c4b371abe9e8dcf0dc2d7374d4140e7f5ad
James Swineson <github@public.swineson.me>
2018-12-11 10:29:21 +0800

    add Han.js render

---
+++ static/assets/common.js
@@ -0,0 +1,9 @@
+(function(fn) {
+    if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+})(function () {
+    Han.init()
+})
